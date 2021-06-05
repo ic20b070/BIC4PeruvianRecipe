17d2baffe5ab6bdf4af951ce8659010dacad6158
ic19b505 <ic19b505@technikum-wien.at>
2021-06-05 22:49:21 +0200

    update design

--- resources/js/components/ingredient/insert_ingredient.vue
+++ resources/js/components/ingredient/insert_ingredient.vue
@@ -1,43 +1,34 @@
 <template>
     <div class="card">
         <div class="card-header">
-            <h1 class="card-header-title is-centered">New Ingredient</h1>
+            <h1 class="card-header-title is-centered">Add Ingredient</h1>
         </div>
         <div class="card-content">
-            <form v-model="form" @submit.prevent="submit">
-                <strong>Name:</strong>
-                <br>
-                <input type="text" style="font-size:20px" class="form-control" v-model="form.name"/>
-                <br>
-                <strong>Description:</strong>
-                <br>
-                <textarea class="form-control" style="height:70px;width:450px; font-size:18px" cols="90" v-model="form.description"/>
-                <br>
-                <strong >Quantity:</strong>
-                <br>
-                <input type="number" style="font-size:18px" class="form-control" v-model="form.quantity"/>
-                <br>
-                <strong>Unit:</strong>
-                <br>
-                <select class="form-control"  style="font-size:18px" v-model="form.unit">
-                    <option value="grams">grams</option>
-                    <option value="milliliters">milliliters</option>
-                    <option value="pieces">pieces</option>
-                </select>
-                <br>
-                <strong>For Recipe:</strong>
-                <br>
-                <select style="font-size:20px" class="form-control" v-model="form.recipe_id">
-                    <option v-for="recipe in recipes" :key="recipe.id" v-bind:value="recipe.id">{{recipe.name}}</option>
-                </select>
-                <hr>
-                <button class="block">Submit</button>
-            </form>
+    <form v-model="form" @submit.prevent="submit">
+        <strong>Name:</strong><br>
+        <input type="text" style="font-size:18px" class="form-control" v-model="form.name"/><br>
+        <strong>Description:</strong><br>
+        <textarea class="form-control" style="height:60px;width:400px; font-size:14px" cols="80" v-model="form.description"/><br>
+        <strong >Quantity:</strong><br>
+        <input type="number" style="font-size:18px" class="form-control" v-model="form.quantity"/><br>
+        <strong>Unit:</strong><br>
+        <select class="form-control"  style="font-size:14px" v-model="form.unit">
+            <option value="grams">grams</option>
+            <option value="milliliters">milliliters</option>
+            <option value="pieces">pieces</option>
+        </select><br>
+        <strong>For Recipe:</strong><br>
+        <select style="font-size:20px" class="form-control" v-model="form.recipe_id">
+            <option v-for="recipe in recipes" :key="recipe.id" v-bind:value="recipe.id">{{recipe.name}}</option>
+        </select>
+        <hr>
+        <button class="block">Submit</button>
+    </form>
         </div>
 
         </div>
 </template>
-
+  //?????????????????????????????????????????????????????????????????????????????????????????????
 <script>
 let form = new Form({
     'id':'',
@@ -48,20 +39,21 @@ let form = new Form({
     'quantity':'',
     'recipe_id':''
 });
+  //?????????????????????????????????????????????????????????????????????????????????????????????
 export default {
     name: "insert_ingredient.vue",
     components:{
         QueryMessage
     },
     created(){
-        console.log("insert_ingredient loaded");
-        axios.get('../list/recipe')
-            .then(response => {
-                this.recipes = response.data;
-                console.log("Inhalt recipies");
-                console.log(this.recipes);
-            })
-            .catch(e => console.log(e));
+	        console.log("insert_ingredient loaded");
+	        axios.get('../list/recipe')
+	            .then(response => {
+	                this.recipes = response.data;
+	                console.log("Inhalt recipies");
+	                console.log(this.recipes);
+	            })
+	            .catch(e => console.log(e));
     },
     data(){
         return{
@@ -70,23 +62,25 @@ export default {
             recipes:[],
         }
     },
+      //?????????????????????????????????????????????????????????????????????????????????????????????
     methods:{
-        submit() {
-            this.url='/ingredient';
-            this.form
-                .post(this.url)
-                .then((response)=>{
-                    console.log(response);
-                })
-                .catch((error)=>{
-                    console.log("Errormessage:");
-                    console.log(this.form.failMessage);
-                });
+submit() {
+    this.url='/ingredient';
+    this.form
+        .post(this.url)
+        .then((response)=>{
+            console.log(response);
+        })
+        .catch((error)=>{
+            console.log("Errormessage:");
+            console.log(this.form.failMessage);
+        });
         }
     }
 
 }
 </script>
+
 
 <style scoped>
 .block {
@@ -95,8 +89,8 @@ export default {
     border: none;
     background-color: #FF5100FF;
     color: white;
-    padding: 14px 28px;
-    font-size: 20px;
+    padding: 12px 24px;
+    font-size: 18px;
     cursor: pointer;
     text-align: center;
 }
